d75fd0cd6385948dfd99c8ef27507e7b77230819
Otiomo <131162694+Otiomo@users.noreply.github.com>
2023-05-04 13:37:43 +0200

    Add files via upload

---
+++ UserManagement/src/com/xadmin/usermanagement/dao/EmployeeDao.java
@@ -0,0 +1,58 @@
+package com.xadmin.usermanagement.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.xadmin.usermanagement.login.Employee;
+public class EmployeeDao {
+
+    public int registerEmployee(Employee employee) throws ClassNotFoundException {
+        String INSERT_USERS_SQL = "INSERT INTO employees" +
+            "  (first_name, last_name, username, password, address, email) VALUES " +
+            " (?, ?, ?, ?, ?,?);";
+
+        int result = 0;
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        try (Connection connection = DriverManager
+            .getConnection("jdbc:mysql://localhost:3307/test?useSSL=false", "root", "Tomate11!");
+
+            // Step 2:Create a statement using connection object
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
+            preparedStatement.setString(1, employee.getFirstName());
+            preparedStatement.setString(2, employee.getLastName());
+            preparedStatement.setString(3, employee.getUsername());
+            preparedStatement.setString(4, employee.getPassword());
+            preparedStatement.setString(5, employee.getAddress());
+            preparedStatement.setString(6, employee.getEmail());
+
+            System.out.println(preparedStatement);
+            // Step 3: Execute the query or update query
+            result = preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            // process sql exception
+            printSQLException(e);
+        }
+        return result;
+    }
+
+    private void printSQLException(SQLException ex) {
+        for (Throwable e: ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
+    }
+}
